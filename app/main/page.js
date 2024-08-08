02296860505a41457eaaca820a298ff6a40ed8b2
Habiba Abdelrehim <habhas2000@gmail.com>
2024-08-08 01:10:02 -0400

    add homepage

---
+++ app/main/page.js
@@ -0,0 +1,7 @@
+"use client";
+import { Box, Button, Typography} from '@mui/material';
+
+export default function Main() {
+    return (<Box> 
+    </Box>);
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,68 @@
-import Image from "next/image";
+"use client";
+import { Box, Button, Typography} from '@mui/material';
+import Link from 'next/link';
+import './globals.css'
 
 export default function Home() {
-  return <></>
+  return (
+    <Box sx={{
+      display: 'flex',
+      flexDirection:'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundImage: 'url(/home.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      position: 'relative',
+      zIndex:0,
+    }}>
+    
+    <Typography variant="h2"  
+      sx={{
+      position: 'relative',
+      zIndex: 2,
+      color: 'black',
+      fontWeight: 'bold',
+      textAlign: 'center',
+  }}> Pantry Tracker
+  </Typography>
+  <Typography variant="h5"  
+      sx={{
+      position: 'relative',
+      zIndex: 2,
+      color: 'black',
+      textAlign: 'center',
+      marginBottom:4,
+  }}> Manage your inventory and get cool recipes !
+  </Typography>
+  <Link href="/main" passHref>
+  <Button
+        variant="contained"
+        sx={{
+          position: 'relative',
+          zIndex: 2,
+          textAlign:'center',
+          backgroundColor: 'green',
+          '&:hover': {
+            backgroundColor: 'darkgreen',
+          },
+        }}
+      >
+        Get Started
+      </Button>
+      </Link>
+    <Box
+      sx={{
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundSize: 'cover',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)', // White color with 50% opacity
+        zindex:'1',
+      }}
+    >
+  </Box>
+  </Box>
+  );
 }
